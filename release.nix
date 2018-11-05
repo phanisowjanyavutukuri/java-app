f5014b78913de29702876953ef8205edfbc5fa09
phanisowjanyavutukuri <32537760+phanisowjanyavutukuri@users.noreply.github.com>
2018-11-05 15:54:51 +0530

    Update release.nix

--- release.nix
+++ release.nix
@@ -1,23 +1,7 @@
-let
-  pkgs = import <nixpkgs> {}; 
-
-  jobs = rec { 
-
-    tarball =  pkgs.releaseTools.sourceTarball { 
-        name = "hello-tarball";
-        src = <hello>; 
-        buildInputs = (with pkgs; [ gettext texLive texinfo ]);
-      };
-
-    build = { system ? builtins.currentSystem }:  
-
-      let pkgs = import <nixpkgs> { inherit system; }; in
-      pkgs.releaseTools.nixBuild { 
-        name = "hello";
-        src = jobs.tarball;
-        configureFlags = [ "--disable-silent-rules" ];
-      };
-  };
-in
-  jobs 
-      
+derivation {
+name = "foo";
+system = "x86 64-linux";
+builder = builtins.toFile "builder.sh"
+’’ mkdir -p "$out"
+echo "Hello, world!" > "$out/some-result"
+’’;
